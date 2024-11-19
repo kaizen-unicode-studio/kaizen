d0b73c0b6905c5a6f55c7e4f7da943d0295cd264
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-11-19 13:23:11 +0200

    change file structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
-import About from "@/components/About";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
+import About from "@/sections/About";
+import Header from "@/sections/Header";
+import Hero from "@/sections/Hero";
 
 export default function Home() {
   return (
