25573a57fc7435606da4c15a3531858f1e6af254
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-11-29 16:47:14 +0200

    hotfix add cantarell local font

--- src/app/style.ts
+++ src/app/style.ts
@@ -3,14 +3,30 @@
 import { css } from "@emotion/react";
 
 export const GlobalStyles = css`
-  @import url("https://fonts.googleapis.com/css2?family=Cantarell:ital,wght@0,400;0,700;1,400;1,700&family=Fredoka:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
   @import url("https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
+
+  @font-face {
+    font-family: "Cantarell";
+    font-style: normal;
+    font-weight: 700;
+    src: url("/fonts/Cantarell-Bold.ttf") format("truetype");
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: "Cantarell";
+    font-style: normal;
+    font-weight: 400;
+    src: url("/fonts/Cantarell-Regular.ttf") format("truetype");
+    font-display: swap;
+  }
 
   @font-face {
     font-family: "Montserrat";
     font-style: normal;
     font-weight: 400;
     src: url("/fonts/Montserrat-Regular.ttf") format("truetype");
+    font-display: swap;
   }
 
   @font-face {
@@ -18,6 +34,7 @@ export const GlobalStyles = css`
     font-style: normal;
     font-weight: 700;
     src: url("/fonts/Montserrat-Bold.ttf") format("truetype");
+    font-display: swap;
   }
 
   @font-face {
@@ -25,6 +42,7 @@ export const GlobalStyles = css`
     font-style: normal;
     font-weight: 600;
     src: url("/fonts/Montserrat-SemiBold.ttf") format("truetype");
+    font-display: swap;
   }
 
   @font-face {
@@ -32,12 +50,14 @@ export const GlobalStyles = css`
     font-style: normal;
     font-weight: 200;
     src: url("/fonts/Montserrat-ExtraLight.ttf") format("truetype");
+    font-display: swap;
   }
   @font-face {
     font-family: "Montserrat";
     font-style: normal;
     font-weight: 500;
     src: url("/fonts/Montserrat-Medium.ttf") format("truetype");
+    font-display: swap;
   }
 
   :root {
