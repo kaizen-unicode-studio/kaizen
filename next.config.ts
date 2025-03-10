8d40429a8219ba549f146f82864b8f34d531adc7
kaizen-unicode-studio <kaizen.project.unicode.studiogmail.com>
2025-03-10 14:16:08 +0200

    update cache control for images and fonts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   compiler: {
     emotion: true,
   },
+  images: {
+    minimumCacheTTL: 60 * 60, // hour
+  },
+  async headers() {
+    return [
+      {
+        source: "/fonts/:all*", // matches any request to /fonts
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
