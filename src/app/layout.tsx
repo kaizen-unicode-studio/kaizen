5fe15fb5d8eb5f5866546c0b95c92f6690925386
kaizen-unicode-studio <kaizen.project.unicode.studiogmail.com>
2025-03-14 10:56:27 +0200

    add meta pixel script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import StyledComponentsRegistry from "./lib/registry";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Kaizen",
@@ -13,6 +14,37 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Meta Pixel Code --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- Meta Pixel Code -->
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '983988200534142');
+fbq('track', 'PageView');
+<!-- End Meta Pixel Code -->
+          `,
+          }}
+        ></script>
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=983988200534142&ev=PageView&noscript=1"
+            alt="meta noscript"
+          />
+        </noscript>
+        {/* <!-- End Meta Pixel Code --> */}
+      </head>
       <body>
         <StyledComponentsRegistry>
           {children}
